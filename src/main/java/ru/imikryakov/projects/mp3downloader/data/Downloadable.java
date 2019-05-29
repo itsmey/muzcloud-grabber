73ea8929d14a85cfa9270c02ea8b5b0f23aa74a5
Ivan Mikryakov <i.mikryakov@premiumitsolution.com>
2019-05-29 11:15:21 +0300

    song downloading

--- src/main/java/ru/imikryakov/projects/mp3downloader/data/Downloadable.java
+++ src/main/java/ru/imikryakov/projects/mp3downloader/data/Downloadable.java
@@ -1,5 +1,11 @@
 package ru.imikryakov.projects.mp3downloader.data;
 
+import ru.imikryakov.projects.mp3downloader.http.HttpClientManager;
+
 public interface Downloadable {
     String getDownloadLink();
+
+    default void download(String directory) {
+        HttpClientManager.getHttpClient().get(getDownloadLink()).asFile(directory);
+    }
 }
