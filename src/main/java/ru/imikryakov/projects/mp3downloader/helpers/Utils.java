fe095b0292ba6764f00b7ca7681b62eae3e26892
Ivan Mic <ivan.micr@gmail.com>
2019-05-31 11:29:45 +0300

    filter unacceptable symbols in song file name

--- src/main/java/ru/imikryakov/projects/mp3downloader/helpers/Utils.java
+++ src/main/java/ru/imikryakov/projects/mp3downloader/helpers/Utils.java
@@ -12,6 +12,6 @@ public class Utils {
     }
 
     public static String prepareFileName(String name) {
-        return name.replaceAll("\\?", "");
+        return name.replaceAll("[\\\\\\/:?*\"<>|+.]", "").trim();
     }
 }
